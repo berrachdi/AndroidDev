df4dceda8cafb398ec44e53920a10675e6e7c24b
Berrachdi Mohamed <75618509+berrachdi@users.noreply.github.com>
2021-04-24 17:32:10 +0200

    chattApp 2.0

--- chatApp.c
+++ chatApp.c
@@ -2,6 +2,10 @@
 int main(){
   int a = 12,b = 13;
   printf(" a = %d",a);
-   printf(" b = %d",b);
+  printf(" b = %d",b);
+  
+  printf(" b = %d",b);
+  printf(" b = %d",b);
+  printf(" b = %d",b);
 
 }
